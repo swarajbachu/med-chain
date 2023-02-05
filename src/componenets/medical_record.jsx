8b5dc7505bfb9db12904be5409d0517f8f53b2f6
swaraj bachu <rajswaraj.r@gmail.com>
2023-02-05 08:25:37 +0530

    tailind issue fixed

--- src/componenets/medical_record.jsx
+++ src/componenets/medical_record.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const MedicalRecords = () => {
   return (
     <div>
-      
+3       <form>
+<input type="text" placeholder="Type here" className="input input-bordered input-primary w-full max-w-xs" />
+</form>
     </div>
   )
 }
