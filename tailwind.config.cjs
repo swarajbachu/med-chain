bcc34ef52913e492558fdc9f0286f0e580453c70
swaraj bachu <rajswaraj.r@gmail.com>
2023-02-05 08:33:44 +0530

    theme settings

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,5 +7,31 @@ module.exports = {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+
+          "primary": "#bde0fe",
+
+          "secondary": "#ffafcc",
+
+          "accent": "#023047",
+
+          "neutral": "#3D4451",
+
+          "base-100": "#FFFFFF",
+
+          "info": "#3ABFF8",
+
+          "success": "#36D399",
+
+          "warning": "#FBBD23",
+
+          "error": "#F87272",
+        },
+      },
+    ],
+  },
   plugins: [require("daisyui")],
 }
