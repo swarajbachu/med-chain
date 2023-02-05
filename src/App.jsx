d8360cf4aaa5c0bf76a73092ce9ed940890d7474
swaraj bachu <rajswaraj.r@gmail.com>
2023-02-05 13:56:24 +0530

    styles changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import { MedicalRecords, MedicalCard, UserForm } from './componenets'
-import { MedicalRecordPage } from './containers'
 import MedChainLandingPage from './containers/MedChainLandingPage'
 
 
-import './App.css'
 
 function App() {
 
   return (
-    <div className="App">
+    <div>
       <MedChainLandingPage/>
     </div>
   )
