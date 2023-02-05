a4669884afe5bdd6cc958a526e5baa56d676163c
swaraj bachu <rajswaraj.r@gmail.com>
2023-02-05 13:12:25 +0530

    lot of changes

--- src/componenets/medical_record.jsx
+++ src/componenets/medical_record.jsx
@@ -1,13 +1,28 @@
 import React from 'react'
 import { useForm } from "react-hook-form";
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
 
 const MedicalRecords = () => {
 
-    const { register, handleSubmit } = useForm();
+    const schema = yup.object().shape({
+        filename: yup.string().required(),
+        doctor_name: yup.string(""),
+        hopital_name: yup.string().required(),
+        tags: yup.string(),
+        file: yup.string().required(),
+    });
+
+    const { register, handleSubmit, formState: {errors} } = useForm({
+        resolver: yupResolver(schema),
+    });
 
     const onSubmit = (data) => {
         console.log(data)
     }
+
+    
+
 
 
     return (
@@ -19,33 +34,26 @@ const MedicalRecords = () => {
                 placeholder="Disease Diagonesed" 
                 className="input input-bordered input-primary m-5 b-full max-w-xs flex" 
                 {...register("filename")}/>
-
+                <p className='pl-6'>{errors.filename?.message}</p>
                 <input type="text" 
                 placeholder="Doctor Consulted" 
                 className="input input-bordered input-primary m-5 w-full max-w-xs flex"
                 {...register("doctor_name")}/>
-
+                <p className='pl-6'>{errors.doctor_name?.message}</p>
                 <input type="text" 
                 placeholder="Hospital" {...register("hopital_name")}
                 className="input input-bordered input-primary m-5 w-full max-w-xs flex" 
                 />
-
+                <p className='pl-6'>{errors.hopital_name?.message}</p>
                 <input type="Decription" 
                 placeholder="Decription" 
                 className="input input-bordered input-primary m-5 w-full max-w-xs flex"
                 {...register("tags")}/>
-                
-            
                 <input type="file" className="file-input file-input-bordered w-full max-w-xs ml-4"  
                 {...register("file")}
                 />
-
                 <button type='submit' className="btn btn-active btn-primary ml-10
                  px-16">Submit</button>
-
-
-                <input type="submit"
-                className="input input-bordered input-primary m-5 w-full max-w-xs flex"/>
 
             </form>
         </div>
