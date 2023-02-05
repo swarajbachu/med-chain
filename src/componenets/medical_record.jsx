3cd1dfb421fdf9b398fdd9f7310e854951f43220
mach-12 <89384052+mach-12@users.noreply.github.com>
2023-02-05 11:44:21 +0530

    added form

--- src/componenets/medical_record.jsx
+++ src/componenets/medical_record.jsx
@@ -1,15 +1,50 @@
 import React from 'react'
+import { useForm } from "react-hook-form";
 
 const MedicalRecords = () => {
+
+    const { register, handleSubmit } = useForm();
+
+    const onSubmit = (data) => {
+        console.log(data)
+    }
+
+
     return (
         <div>
-            <form>
+            <form onSubmit={handleSubmit(onSubmit)}>
+
+
                 <input type="text" 
+                placeholder="Enter name of the file" 
+                className="input input-bordered input-primary m-5 b-full max-w-xs flex" 
+                {...register("filename")}/>
+
+                <input type="text" 
+                placeholder="Issued by doctor" 
+                className="input input-bordered input-primary m-5 w-full max-w-xs flex"
+                {...register("doctor_name")}/>
+
+                <input type="text" 
+                placeholder="Issued by hospital" {...register("hopital_name")}
+                className="input input-bordered input-primary m-5 w-full max-w-xs flex" 
+                />
+
+                <input type="text" 
+                placeholder="Tags comma seperated" 
+                className="input input-bordered input-primary m-5 w-full max-w-xs flex"
+                {...register("tags")}/>
+                
+                <input type="file" 
                 placeholder="Type here" 
-                className="input input-bordered input-primary w-full max-w-xs" />
+                className="input input-bordered input-primary m-5 w-full max-w-xs flex" 
+                {...register("file")}/>
+
+
             </form>
         </div>
     )
 }
 
 export default MedicalRecords
+
